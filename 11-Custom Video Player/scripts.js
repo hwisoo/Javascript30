984bc3d045134624224fd96859e8159b94cdf9b2
James Cho <hwisoocho@gmail.com>
2018-10-06 23:18:17 -0700

    togglePlay()

--- 11-Custom Video Player/scripts.js
+++ 11-Custom Video Player/scripts.js
@@ -3,9 +3,14 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /*Build out Functions */
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+/* Hook up the event listeners */
